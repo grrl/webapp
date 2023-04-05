fa170a8490dc0c7ed46c177bccccbc8ec8edeed5
your name <you@example.com>
2023-04-05 14:05:30 +0300

    .

--- connect.php
+++ connect.php
@@ -37,7 +37,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
 <!-- Navbar -->
 <div class="w3-top">
-  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+  <div class="w3-bar w3-sand w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="connect.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
     <a href="order.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Order</a>
@@ -56,10 +56,13 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 </div>
 
 <!-- Header -->
-<header class="w3-container w3-red w3-center" style="padding:128px 16px">
-  <h1 class="w3-margin w3-jumbo">Luonnonvoima</h1>
-  <p class="w3-xlarge">Chocolate Mushroom bars</p>
+<header class="w3-container w3-sand w3-center" style="padding:128px 16px">
+  <img src="logo.png" alt="Paris" width="200" height="200" style="margin-top: -80px;">
+  <div style="margin-top: -80px;">
+  <h1 class="w3-margin w3-xxlarge">Luonnonvoima</h1>
+  <p class="w3-large">Chocolate Mushroom bars</p>
   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Order Now</button>
+  </div>
 </header>
 
 <!-- First Grid -->
