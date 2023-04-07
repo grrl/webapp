a64d8a2653e2c988585f19908fb3aee647b0c754
myname <email>
2023-04-07 18:17:50 +0300

    .

--- process.php
+++ process.php
@@ -100,6 +100,42 @@ foreach ($_POST as $param_name => $param_val) {
     }
 }
 
+function clean($string) {
+    //$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+    $utf8 = array(
+        '/[áàâãªä]/u'   =>   'a',
+        '/[ÁÀÂÃÄ]/u'    =>   'A',
+        '/[ÍÌÎÏ]/u'     =>   'I',
+        '/[íìîï]/u'     =>   'i',
+        '/[éèêë]/u'     =>   'e',
+        '/[ÉÈÊË]/u'     =>   'E',
+        '/[óòôõºö]/u'   =>   'o',
+        '/[ÓÒÔÕÖ]/u'    =>   'O',
+        '/[úùûü]/u'     =>   'u',
+        '/[ÚÙÛÜ]/u'     =>   'U',
+        '/ç/'           =>   'c',
+        '/Ç/'           =>   'C',
+        '/ñ/'           =>   'n',
+        '/Ñ/'           =>   'N',
+        '/–/'           =>   '-', // UTF-8 hyphen to "normal" hyphen
+        '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+        '/[“”«»„]/u'    =>   ' ', // Double quote
+        '/ /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+    );
+
+    preg_replace(array_keys($utf8), array_values($utf8), $string);
+    //$string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+} 
+
+clean($group);
+clean($company);
+clean($country);
+clean($comment);
+clean($regular);
+clean($special);
+
 $basictotal = $regular * 736.8;
 $specialtotal = $special * 840;
 $total = $basictotal + $specialtotal;
