5c921ec5cc26d9b07cfdc07c8332444075c3d7b2
myname <email>
2023-04-09 01:15:02 +0300

    .

--- connect.php
+++ connect.php
@@ -82,8 +82,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
       -->
       <p class="w3-text-grey">4-8-2023
       <p>Dear Customer,<br><br>
-Are you bored selling the same chocolates that are found in any store? Then come down to Luonnonvoima, we take pleasure in introducing to you our mushroom chocolate snacks. These are unique products that are made from functional mushroom powder ingredients to maintain the health of the body and mind.<br>
-Our mushroom chocolate bar is a delicious treat that combines the rich flavor of chocolate with the nutritious health benefits of mushrooms. It's gluten-free and eco-friendly. Perfect for anyone looking for a healthier alternative to traditional chocolate bars. Our research indicates that our business is unique and will offer a significant advantage over all available similar and competing products. We have tested the product and it has passed the relevant certifications. We have not received any complaints about side effects.<br>
+Are you bored selling the same chocolates that are found in any store? Then come down to Luonnonvoima, we take pleasure in introducing to you our mushroom chocolate snacks. These are unique products that are made from functional mushroom powder ingredients to maintain the health of the body and mind.<br><br>
+Our mushroom chocolate bar is a delicious treat that combines the rich flavor of chocolate with the nutritious health benefits of mushrooms. It's gluten-free and eco-friendly. Perfect for anyone looking for a healthier alternative to traditional chocolate bars. <br><br>
+Our research indicates that our business is unique and will offer a significant advantage over all available similar and competing products. We have tested the product and it has passed the relevant certifications. We have not received any complaints about side effects.<br>
 Our snacks will help mitigate health conditions such as:<br>
 ● Alzheimer's<br>
 ● Heart disease<br>
@@ -94,7 +95,7 @@ Our snacks will help mitigate health conditions such as:<br>
 
 <p>We are sure that once your customers try our chocolate, they'll crave more. So, wait no more! Send us your form today! You'll be more than glad that you did!</p>
 
-<p>We can be reached via email or over the phone. We look forward to a long business relationship. <br>
+<p>You may reach us via email or over the phone. We look forward to a long business relationship. <br>
 <br>
 Best regards,<br>
 Bilal Ktea<br>
