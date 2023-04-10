528afcf4600623f7c0e5bb2d48d9abe1da147726
myname <email>
2023-04-10 12:30:25 +0300

    .

--- video.php
+++ video.php
@@ -72,8 +72,7 @@ tr:nth-child(even) {
         <source src="movie.mp4" type="video/mp4">
         <source src="movie.ogg" type="video/ogg">
         Your browser does not support the video tag.
-</video>
-
+        </video>
     </div>
   </div>
 </div>
